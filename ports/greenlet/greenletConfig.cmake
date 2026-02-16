c45506596d044246f0bb890d5ee2f9b8bd6509d9
ccptoebeans <>
2026-02-16 13:24:00 +0000

    Move .so file to lib when building for macos. Add the python path to a custom target property

--- ports/greenlet/greenletConfig.cmake
+++ ports/greenlet/greenletConfig.cmake
@@ -12,7 +12,7 @@ endif()
 
 if(APPLE)
     set_target_properties(Greenlet PROPERTIES
-        IMPORTED_LOCATION "${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/bin/_greenlet.so"
+        IMPORTED_LOCATION "${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/lib/_greenlet.so"
     )
 elseif(WIN32)
     set_target_properties(Greenlet PROPERTIES
@@ -22,7 +22,10 @@ else()
     message(FATAL_ERROR "Greenlet not supported on platform.")
 endif()
 
+define_property(TARGET PROPERTY PYTHON_MODULE_DIR BRIEF_DOCS "path to the python module ")
+
 set_target_properties(Greenlet PROPERTIES
     INTERFACE_INCLUDE_DIRECTORIES "${Greenlet_INCLUDE_DIR}"
     INTERFACE_LINK_LIBRARIES ${Greenlet_Libraries}
+    PYTHON_MODULE_DIR "${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/bin/python"
 )
